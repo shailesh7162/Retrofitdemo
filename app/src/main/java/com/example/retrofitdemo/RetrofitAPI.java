7dbed4e7582b06617d1178a7e22de49b09423328
DELL <shaileshmakwana8000@gmail.com>
2023-06-14 08:36:04 -0700

    1

---
+++ app/src/main/java/com/example/retrofitdemo/RetrofitAPI.java
@@ -0,0 +1,13 @@
+package com.example.retrofitdemo;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface RetrofitAPI
+{
+    @FormUrlEncoded
+    @POST("register.php")
+    Call<Register_user> RegisterUser(@Field("name") String name, @Field("email") String email, @Field("password") String password);
+}
